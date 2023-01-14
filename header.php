5a901416a49c043e94971eaae2cb7d607beb83b5
asifulmamun <asifulmamun@gmail.com>
2023-01-15 00:23:43 +0600

    update]

--- header.php
+++ header.php
@@ -35,7 +35,7 @@
 			</div><!-- .site-branding -->
 
 			<nav id="site-navigation" class="main-navigation">
-				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e('Primary Menu', 'tws-master-pro'); ?></button>
+				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="true"><?php esc_html_e('Primary Menu', 'tws-master-pro'); ?></button>
 				<?php
 				wp_nav_menu(
 					array(
