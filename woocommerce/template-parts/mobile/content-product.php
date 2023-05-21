4149b25116d1a3a4bad183e397808b6dc84004d3
asifulmamun <asifulmamun@gmail.com>
2023-05-21 19:52:21 +0600

    loop for phone ok

--- woocommerce/template-parts/mobile/content-product.php
+++ woocommerce/template-parts/mobile/content-product.php
@@ -9,10 +9,15 @@ if ( empty( $product ) || ! $product->is_visible() ) {
 	return;
 }
 ?>
-<li <?php wc_product_class( 'tws__product_wrapper group relative h-72 mb-10 overflow-hidden grid justify-items-center p-1 hover:shadow-lg', $product ); ?>>
+<li <?php wc_product_class( 'tws__product_wrapper group relative pt-2 pb-3 mb-20 hover:shadow-lg', $product ); ?>>
 	<?php woocommerce_show_product_loop_sale_flash(); /* Onsale */ ?>
-	<a class="h-40 overflow-hidden flex p-2" href="<?php echo get_the_permalink(get_the_ID()); ?>"><img class="max-h-full m-auto" src="<?php echo get_the_post_thumbnail_url(get_the_ID(), 'medium'); ?>" alt="<?php echo get_post_meta(get_post_thumbnail_id(), '_wp_attachment_image_alt', TRUE); /* thumbnail */ ?>"></a>
-	<div class="max-h-20 overflow-hidden pt-3 px-2"><?php woocommerce_template_loop_product_title(); /* Product Title Title */ ?></div>
-	<div class="max-h-7 px-4 overflow-hidden flex items-center bg-yellow-200"><?php do_action('woocommerce_after_shop_loop_item_title'); /* Price */ ?></div>
-	<span class=""><?php do_action( 'woocommerce_after_shop_loop_item' ); /* Add To Cart */ ?></span>
+	<a class="block overflow-hidden text-center" href="<?php echo get_the_permalink(get_the_ID()); ?>">
+		<div class="h-40 overflow-hidden flex">
+			<img class="max-h-full m-auto" src="<?php echo get_the_post_thumbnail_url(get_the_ID(), 'medium'); ?>" alt="<?php echo get_post_meta(get_post_thumbnail_id(), '_wp_attachment_image_alt', TRUE); /* thumbnail */ ?>">
+		</div>
+		<div class="max-h-24 overflow-hidden pt-5 px-2"><?php woocommerce_template_loop_product_title(); /* Product Title Title */ ?></div>
+		<span class="max-h-8 inline-block mt-2 py-2 px-4 overflow-hidden bg-yellow-200"><?php do_action('woocommerce_after_shop_loop_item_title'); /* Price */ ?></span>
+	</a>
+	
+	<span class="tws__add_cat_wrap relative max-h-14 mr-2 grid justify-items-center"><?php do_action( 'woocommerce_after_shop_loop_item' ); /* Add To Cart */ ?></span>
 </li>
