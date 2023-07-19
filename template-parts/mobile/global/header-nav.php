9f14464c37f98887a1b462d7fa5ef6d8f69946cd
asifulmamun <asifulmamun@gmail.com>
2023-07-19 21:28:00 +0600

    aside fixed

--- template-parts/mobile/global/header-nav.php
+++ template-parts/mobile/global/header-nav.php
@@ -18,7 +18,7 @@
     </div>
     <div class="col-span-2 flex tws__site_branding">
         <a href="<?php echo esc_url(home_url('/')); ?>"><svg class="w-6 h-6 scale-125" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" /></svg></a>&nbsp;&nbsp;&nbsp;
-        <button id="toggle_primary_menu" class="border p-1 ml-2">
+        <button id="toggle_primary_menu" class="p-1 ml-2">
             <svg class="h-5 w-5" viewBox="0 0 20 20">
                 <title>Menu</title>
                 <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
--- woocommerce/checkout/form-checkout.php
+++ woocommerce/checkout/form-checkout.php
@@ -33,7 +33,7 @@ if ( ! $checkout->is_registration_enabled() && $checkout->is_registration_requir
 	<?php if ( $checkout->get_checkout_fields() ) : ?>
 		<?php do_action( 'woocommerce_checkout_before_customer_details' ); ?>
 		<div class="grid grid-cols-12 col2-set">
-			<div class="col-span-7 col-1">
+			<div class="col-span-12 md:col-span-7 col-1">
 				<div class="billing_shipping" id="customer_details">
 					<div class="billing">
 						<?php do_action( 'woocommerce_checkout_billing' ); ?>
@@ -45,7 +45,7 @@ if ( ! $checkout->is_registration_enabled() && $checkout->is_registration_requir
 				</div>
 			</div>
 
-			<div class="col-span-5 col-2">
+			<div class="col-span-12 md:col-span-5 col-2">
 				<?php do_action( 'woocommerce_checkout_before_order_review_heading' ); ?>
 				<h3 id="order_review_heading"><?php esc_html_e( 'Your order', 'woocommerce' ); ?></h3>
 				
