ca78184a8c3ecf70ab32fd281aab90c049196c4f
asifulmamun <asifulmamun@gmail.com>
2023-07-31 01:47:27 +0600

    ajax search working

--- template-parts/mobile/global/footer.php
+++ template-parts/mobile/global/footer.php
@@ -1,11 +1,11 @@
 <footer class="fixed bottom-0 right-0 w-full overflow-hidden">
     <div class="grid grid-cols-5 gap-1">
-        <a class="w-full py-2" href="#">
+        <a class="w-full py-2" href="<?php echo get_permalink( get_option('woocommerce_myaccount_page_id') ); ?>&orders">
             <svg class="w-6 mx-auto" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z" />
             </svg>
         </a>
-        <a class="w-full py-2" href="#">
+        <a class="w-full py-2" href="<?php echo esc_url(home_url('/')); ?>?s=">
             <svg class="w-6 mx-auto" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
             </svg>
@@ -25,7 +25,7 @@
                 <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
             </svg>
         </a>
-        <a class="w-full py-2" href="#">
+        <a class="w-full py-2" href="<?php echo get_permalink( get_option('woocommerce_myaccount_page_id') ); ?>">
             <svg class="w-6 mx-auto" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
             </svg>
