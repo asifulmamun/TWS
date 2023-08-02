2a890a64ea7e6157c868a67316b8ee941c84e0e0
asifulmamun <asifulmamun@gmail.com>
2023-08-02 22:35:55 +0600

    Release version 1.6

--- assets/src/js/abstract/global.js
+++ assets/src/js/abstract/global.js
@@ -4,23 +4,47 @@
 
 console.log('This is global.js');
 
-const xmlhttp = new XMLHttpRequest();
-xmlhttp.onload = function() {
-	var data = JSON.parse(this.responseText);
-	console.log(data);
 
-	// Top Search Place Holder
+// static data update for mobile and desktop
+function update_static_data(data) {
+	// Update Top Search Placeholder
 	let tws__top_search = document.getElementById('tws__top_search');
-	if(tws__top_search){
+	if (tws__top_search) {
 		tws__top_search.placeholder = data.tws__top_search;
 	}
 
-	// Footer Live Chat Change
+	// Update Footer Live Chat Text
 	let tws__live_support = document.getElementById('tws__live_support');
-	if(tws__live_support){
+	if (tws__live_support) {
 		tws__live_support.innerText = data.tws__live_support;
 	}
-
 }
-xmlhttp.open("GET", tws__site_root+'/inc/data.json');
+
+// static data get from json files and update data as per terms and conditions
+const xmlhttp = new XMLHttpRequest();
+xmlhttp.onload = function () {
+	if (xmlhttp.status === 200) {
+		var data = JSON.parse(this.responseText);
+		update_static_data(data);
+		
+	} else {
+		// If data.json not found, try to request data-sample.json
+		const xmlhttpSample = new XMLHttpRequest();
+		xmlhttpSample.onload = function () {
+			if (xmlhttpSample.status === 200) {
+				var data = JSON.parse(xmlhttpSample.responseText);
+				update_static_data(data);
+			} else {
+				console.log("data-sample.json not found or returned an error.");
+			}
+		};
+
+		// Send the HTTP GET request to retrieve the data-sample.json
+		xmlhttpSample.open("GET", tws__site_root + '/inc/data-sample.json');
+		xmlhttpSample.send();
+	}
+};
+
+// Send the HTTP GET request to retrieve the data.json
+xmlhttp.open("GET", tws__site_root + '/inc/data.json');
 xmlhttp.send();
--- template-parts/desktop/global/header-nav.php
+++ template-parts/desktop/global/header-nav.php
@@ -19,7 +19,7 @@
                     }
                     // Thu Custom Logo
             ?></div>
-            <form action="">
+            <form action="<?php echo esc_url(home_url('/')); ?>">
                 <input id="tws__top_search" name="s" type="search" class="w-full block pl-20 group-hover:pl-4 pr-10 py-2 rounded outline-none transition-all ease-in-out delay-150 duration-300" placeholder="Search" />
                 <button type="submit" class="absolute inset-y-0 right-3 flex items-center">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" /></svg>
